81687e4c3a7225a0e7498c39d41acc49a7dd913b
MohammadKhalid <khalid.180695@gmail.com>
2019-09-23 23:24:57 +0500

    event args

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const emitter = new EventEmitter();
 
 //Register a listener
 //emitter.addListener() = emitter.on()
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
+});
+
+emitter.on('logging', (arg) => {
+    console.log('Logging Listener called', arg);
 });
 
 //Raise an event
-emitter.emit('messageLogged');
+//emitter.emit('messageLogged', {id: 1, url:'http://'});
+
+//Raise: logging (data: message)
+emitter.emit('logging', {message: 'abc'});
