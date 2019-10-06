c7930325a9b6cf612f522ac31265b771a919ebe5
MohammadKhalid <khalid.180695@gmail.com>
2019-10-06 20:41:07 +0500

    callback hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,23 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Waiting');
-}, 2000);
+getUser(1, (user) => {
+    console.log('User', user);
+    //get repos
+    getRepositories(user.username, (repos) => {
+        console.log('User repos', repos);
+    });
+});
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Getting user...');
+        callback({id: id, username:'khalid'});
+    }, 2000);
+};
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Getting repos...')
+        callback({username: username, repos:['repo1', 'repo2', 'repo3']});
+    }, 2000);
+}
