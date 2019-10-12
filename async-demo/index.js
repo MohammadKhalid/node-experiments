5b6af5fc53d541a3beff2c255b7511ad7949daee
MohammadKhalid <khalid.180695@gmail.com>
2019-10-12 23:03:05 +0500

    exercise

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,4 +1,6 @@
 console.log('Before');
+
+// Callback approach
 // getUser(1, (user) => {
 //   getRepositories(user.gitHubUsername, (repos) => {
 //     getCommits(repos[0], (commits) => {
@@ -7,13 +9,29 @@ console.log('Before');
 //   })
 // });
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
+//Promise approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+//Async and Await approach
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
 
 console.log('After');
+
 
 function getUser(id) {
     return new Promise((resolve, reject) => {
